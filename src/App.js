63d96191e0c04baccba80f8cb36ba099bd1a5c14
Kerem Burak Yılmaz <kyilmaz22@ku.edu.tr>
2025-12-29 14:35:54 +0100

    remove spotify playback

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Projects from './components/Projects/Projects';
 import Resume from './components/Resume/Resume';
 import Contact from './components/Contact/Contact';
 import SpotifyBrain from './components/SpotifyBrain/SpotifyBrain';
-import { SpotifyPlayerProvider } from './contexts/SpotifyPlayerContext';
 
 export default function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -37,7 +36,7 @@ export default function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/spotify-brain" element={<SpotifyPlayerProvider><SpotifyBrain /></SpotifyPlayerProvider>} />
+          <Route path="/spotify-brain" element={<SpotifyBrain />} />
         </Routes>
       </main>
     </div>
